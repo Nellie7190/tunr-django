93b8dc318795583ba51692fe70df074117151518
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-11-04 12:17:56 -0400

    fixed home url

--- tunr/urls.py
+++ tunr/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
+    path('', views.ArtistList.as_view(), name='artist_list'),
     path('artists/', views.ArtistList.as_view(), name='artist_list'),
     path('artists/<int:pk>', views.ArtistDetail.as_view(), name='artist_detail'),
     path('songs/', views.SongList.as_view(), name='song_list'),
